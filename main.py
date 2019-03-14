1bed47d2b97d52dc2c5fe8d592cf8785c47dd17a
Isaac Rehg <isaacrehg@gmail.com>
2019-03-13 18:43:33 -0700

    add option to change activation

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import pickle
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, activation):
         super(Net, self).__init__()
+        self.activation = activation
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(4 * 4 * 50, 500)
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        x = self.activation(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
+        x = self.activation(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
         x = x.view(-1, 4 * 4 * 50)
-        x = F.relu(self.fc1(x))
+        x = self.activation(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    losses = []
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -36,9 +39,11 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
+            losses += [loss.item()]
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
+    return losses
 
 
 def test(args, model, device, test_loader):
@@ -59,10 +64,23 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+    return test_loss, 100. * correct / len(test_loader.dataset)
+
+
+def save_file(args, model):
+    # save to different path depending on args
+    path = "mnist_cnn"
+    if args.activation == 'relu':
+        path += '_sigmoid'
+    path += '.pt'
+    torch.save(model.state_dict(), save_file)
+
 
 def main():
-    # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    # My Settings
+    parser.add_argument('--activation', type=str, choices=['relu', 'sigmoid'], default='relu')
+    # Training settings
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
@@ -104,15 +122,29 @@ def main():
         ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-    model = Net().to(device)
+    activation = F.relu if args.activation == 'relu' else F.sigmoid
+
+    model = Net(activation).to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
+    train_losses = []
+    val_losses = []
+    val_accuracies = []
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        train_losses += train(args, model, device, train_loader, optimizer, epoch)
+        val_loss, val_accuracy = test(args, model, device, test_loader)
+        val_losses += [val_loss]
+        val_accuracies += [val_accuracy]
 
-    if (args.save_model):
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    with open('train-loss.pkl', 'wb+') as f:
+        pickle.dump(train_losses, f)
+    with open('val-accuracy.pkl', 'wb+') as f:
+        pickle.dump(val_accuracies, f)
+    with open('val-loss.pkl', 'wb+') as f:
+        pickle.dump(val_loss, f)
+
+    if args.save_model:
+        save_file(args, model)
 
 
 if __name__ == '__main__':
